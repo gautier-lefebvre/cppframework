f4c2dac89ae00967f6892a53779333277aeb9d34
Gautier Lefebvre <gautier.lfbvr@gmail.com>
2015-12-21 23:16:49 +0100

    release 0.1.0

---
+++ framework/headers/Core/Meta.hh
@@ -0,0 +1,12 @@
+#ifndef   __CORE_META_HH__
+#define   __CORE_META_HH__
+
+#include  <string>
+
+namespace   Core {
+  namespace Meta {
+    static const std::string version = "0.1.0";
+  }
+}
+
+#endif    /* __CORE_META_HH__ */
