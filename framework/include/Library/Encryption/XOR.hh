7059b77766d79d0480f3f415ee210bb74f698e47
Gautier Lefebvre <gautier.lfbvr@gmail.com>
2016-02-03 19:58:10 +0100

    xor refactor

--- framework/include/Library/Encryption/XOR.hh
+++ framework/include/Library/Encryption/XOR.hh
@@ -3,55 +3,21 @@
 
 #include  <cstdint>
 
-#include  "Library/DesignPattern/Singleton.hpp"
-
 namespace  Encryption {
   /**
    *  \class XOR Library/Encryption/XOR.hh
    *  \brief a class which implements a XOR encryption algorithm (a^b).
    */
-  class  XOR :public Singleton<Encryption::XOR> {
-    friend class Singleton<Encryption::XOR>;
-  private:
-    uint8_t    *_key; /*!< pointer to the start of the encryption key */
-    size_t    _length; /*!< size of the encryption key in bytes */
-
-  private:
-    /**
-     *  \brief Constructor of XOR.
-     *
-     *  The key is set to \a nullptr.
-     */
-    XOR(void);
-
-    /**
-     *  \brief Destructor of XOR.
-     */
-    virtual ~XOR(void);
-
+  class  XOR {
   public:
     /**
-     *  \brief Sets the encryption key.
-     *  \param ptr pointer to the start of the encryption key.
-     *  \param size size of the encryption key in bytes.
-     */
-    void  setKey(const void* ptr, size_t size);
-
-    /**
      *  \brief Encrypts data with the current encryption key.
-     *
-     *  If the key is not set, does nothing.
-     *
-     *  \param addr a pointer to the start of the data to encrypt.
-     *  \param size size of the data to encrypt in bytes.
+     *  \param data a pointer to the start of the data to encrypt.
+     *  \param dataSize size of the data to encrypt in bytes.
+     *  \param key a pointer to the start of the key used to encrypt.
+     *  \param keySize size of the key in bytes.
      */
-    void  xorify(void* addr, size_t size) const;
-
-  private:
-    /**
-     *  \brief Resets the key to \a nullptr.
-     */
-    void  clear(void);
+    static void  xorify(void* data, size_t dataSize, const void* key, size_t keySize);
   };
 }
 
--- framework/src/Library/Encryption/XOR.cpp
+++ framework/src/Library/Encryption/XOR.cpp
@@ -2,35 +2,12 @@
 
 #include  "Library/Encryption/XOR.hh"
 
-Encryption::XOR::XOR(void):
-  _key(nullptr),
-  _length(0)
-{}
-
-Encryption::XOR::~XOR(void) {
-  this->clear();
-}
-
-void  Encryption::XOR::clear(void) {
-  if (this->_key != nullptr) {
-    delete[] (this->_key);
-  }
-  this->_key = nullptr;
-  this->_length = 0;
-}
-
-void  Encryption::XOR::setKey(const void *ptr, size_t length) {
-  this->clear();
-  this->_key = new uint8_t[length];
-  this->_length = length;
-  memcpy(this->_key, reinterpret_cast<const uint8_t*>(ptr), length);
-}
-
-void  Encryption::XOR::xorify(void *addr, size_t size) const {
-  if (this->_key != nullptr && this->_length != 0) {
-    uint8_t* target = reinterpret_cast<uint8_t*>(addr);
-    for (size_t i = 0 ; i < size ; ++i) {
-      target[i] ^= this->_key[i % this->_length];
+void  Encryption::XOR::xorify(void *data, size_t dataSize, const void* key, size_t keySize) {
+  if (key != nullptr && data != nullptr && dataSize > 0 && keySize > 0) {
+    uint8_t* dataPtr = reinterpret_cast<uint8_t*>(data);
+    const uint8_t* keyPtr = reinterpret_cast<const uint8_t*>(key);
+    for (size_t i = 0 ; i < dataSize ; ++i) {
+      dataPtr[i] ^= keyPtr[i % keySize];
     }
   }
 }
