a483eb28b61112e327752d1076e7b53dcbc2a405
Gautier Lefebvre <gautier.lfbvr@gmail.com>
2016-02-12 12:20:54 +0100

    add documentation for DelayedTasksThread

--- framework/include/Core/Worker/DelayedTasksThread.hh
+++ framework/include/Core/Worker/DelayedTasksThread.hh
@@ -8,27 +8,58 @@
 #include  "Library/Threading/Lockable.hpp"
 
 namespace fwk {
+  /**
+   *  \class DelayedTasksThread Core/Worker/DelayedTasksThread.hh
+   *  \brief Singleton class running on a thread and adding delayed tasks to the task queue.
+   */
   class DelayedTasksThread :public Singleton<fwk::DelayedTasksThread>, public Lockable, public AEndable {
     friend class Singleton<fwk::DelayedTasksThread>;
   private:
-    std::thread *_thread;
+    std::thread *_thread; /*!< the thread on which the class runs. */
 
   private:
+    /**
+     *  \brief Deleted copy constructor of DelayedTasksThread.
+     */
     DelayedTasksThread(const DelayedTasksThread&) = delete;
+
+    /**
+     *  \brief Deleted move constructor of DelayedTasksThread.
+     */
     DelayedTasksThread(const DelayedTasksThread&&) = delete;
+
+    /**
+     *  \brief Deleted assignment constructor of DelayedTasksThread.
+     */
     DelayedTasksThread& operator=(const DelayedTasksThread&) = delete;
 
   private:
+    /**
+     *  \brief Constructor of DelayedTasksThread.
+     */
     DelayedTasksThread(void);
+
+    /**
+     *  \brief Destructor of DelayedTasksThread. Ends the thread.
+     */
     virtual ~DelayedTasksThread(void);
 
   public:
+    /**
+     *  \brief Notifies the thread to stop and waits for it to end.
+     */
     virtual void  end(void);
 
   public:
+    /**
+     *  \brief Launch the thread.
+     */
     void  run(void);
 
   private:
+    /**
+     *  \brief Routine run by the thread. Adds DelayedTask objects to the task queue when the delay is over. Ends whenever the end() method is called.
+     */
     void  routine(void) const;
   };
 }
