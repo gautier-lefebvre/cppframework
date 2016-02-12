08bd9b6416fc78714708eb12cc8004a0d279af3b
Gautier Lefebvre <gautier.lfbvr@gmail.com>
2016-02-12 14:05:19 +0100

    fix headerify script

--- framework/headerify.py
+++ framework/headerify.py
@@ -6,14 +6,51 @@ sys.path.append(os.path.dirname(__file__))
 import re
 import traceback
 
+class Content:
+  def __init__(self, lines):
+    self.lines = lines
+
+  def epure(self):
+    """ Remove all comments. """
+
+    content = ""
+    for line in self.lines:
+      content += line
+
+    # remove all /* */ comments
+    start_pos = content.find("/*")
+    while start_pos != -1:
+      end_pos = content.find("*/", start_pos)
+      content = content[:start_pos] + content[end_pos+2:]
+      start_pos = content.find("/*")
+
+    # remove all // comments
+    start_pos = content.find("//")
+    while start_pos != -1:
+      end_pos = content.find("\n", start_pos)
+      content = content[:start_pos] + content[end_pos:]
+      start_pos = content.find("//")
+
+    lines = []
+
+    self.lines = content.split("\n")
+    for line in self.lines.copy():
+      line = line.rstrip()
+      if line and not line.isspace():
+        lines.append(line)
+
+    self.lines = lines
+
 class Prog:
   def __init__(self, folder, header):
     self.files_included = []
     self.folder_path = folder
-    self.header = open(header, "w")
-    self.regex = re.compile(r'#include[ \t]+"(?P<path>[a-zA-Z\/\.]+)"')
+    self.header = open(header, "w+")
+    self.includeRegex = re.compile(r'#include[ \t]+"(?P<path>[a-zA-Z\/\.]+)"')
+    self.commentRegex = re.compile(r'\/\*.*\*\/')
 
   def clean(self):
+    print("CLOSING FILE")
     self.header.close()
 
   def endswith(self, pattern, extensions):
@@ -40,9 +77,14 @@ class Prog:
     self.files_included.append(file)
 
     with open(file, "r") as f:
-      for line in f.readlines():
+      content = Content(f.readlines())
+
+    content.epure()
+
+    for line in content.lines:
+      if line and not line.isspace():
         try:
-          inc = self.regex.search(line)
+          inc = self.includeRegex.search(line)
           if inc is not None:
             inc = inc.group("path")
         except IndexError:
@@ -50,11 +92,16 @@ class Prog:
           pass
 
         if inc is None:
-          self.header.write(line)
+          if not line.endswith("\n"):
+            line += "\n"
+          try:
+            self.header.write(line)
+          except:
+            traceback.print_exc()
         else:
           self.insert_file(os.path.join(self.folder_path, inc))
 
-      self.header.write("\n")
+    self.header.write("\n")
 
   def headerify(self):
     file = self.find_first_unincluded_file()
@@ -71,9 +118,10 @@ if __name__ == "__main__":
   prog = Prog(av[1] if ac >= 2 else "./include", av[2] if ac >= 3 else "./dist/cppframework.hh")
 
   try:
-    os._exit(prog.headerify())
+    retCode = prog.headerify()
   except:
     traceback.print_exc()
-    sys.exit(1)
-  finally:
-    prog.clean()
+    retCode = 1
+
+  prog.clean()
+  sys.exit(retCode)
